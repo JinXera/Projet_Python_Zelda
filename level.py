fa2ad0826d1756d42ddeceda5d65f845048e8cfe
JinXera <baptiste.salvio@stjoavignon.com>
2024-03-21 10:00:30 +0100

    Premier commit, mise en place du projet sur GitHub

---
+++ level.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Level :
+    def __init__(self):
+        # sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+    def run(self):
+        # update and draw the game
+        pass
