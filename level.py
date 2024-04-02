cc4ca1c582710ece538ca3b58d0932510ea605f7
JinXera <baptiste.salvio@stjoavignon.com>
2024-04-02 17:08:35 +0200

    Ajout de la classe player + modification dans la classe tile et level

--- level.py
+++ level.py
@@ -1,13 +1,28 @@
 import pygame
+from settings import *
+from tile import Tile
+from player import Player
 
 class Level :
     def __init__(self):
 
         # get the display surface
         self.display_surface = pygame.display.get_surface()
+
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'X':
+                    Tile((x,y),[self.visible_sprites])
+
 
     def run(self):
         # update and draw the game
