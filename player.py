dc17d70a3aebc23327582b562c0fe4ba38d2fd1d
JinXera <baptiste.salvio@stjoavignon.com>
2024-04-04 17:15:19 +0200

    avancement player.py, ajout de la vitesse et le déplacement du joueur fonctionne

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2()
+        self.speed = 5
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -26,5 +27,8 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+    def move(self, speed):
+        self.rect.center += self.direction * speed
     def update(self):
         self.input()
+        self.move(self.speed)
