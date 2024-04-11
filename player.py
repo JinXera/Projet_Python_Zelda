d235ff5bd2f9b1f5fe9f50fb6b851492fcb50f24
JinXera <baptiste.salvio@stjoavignon.com>
2024-04-11 17:25:49 +0200

    Animation du player dans player.py en cours, il faut maintenant faire une importation des fichiers pour accéder aux desssins

--- player.py
+++ player.py
@@ -7,14 +7,30 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('../Projet_Python_Zelda/graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
 
+        #graphics setup
+        self.import_player_assets()
+
+        # movement
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.attacking = False
+        self.attack_cooldown = 400
+        self.attack_time = None
 
         self.obstacle_sprites = obstacle_sprites
 
+    def import_player_assets(self):
+        character_path = '../Projet_Python_Zelda/graphics/player/'
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                          'right_idle':[], 'left_idle':[], 'up_idle':[], 'down_idle':[],
+                          'right_attack':[], 'left_attack':[], 'up_attack':[], 'down_attack':[]}
+        for animation in self.animations.keys():
+            full_path = character_path + animation*
+           # self.animations[animation] = import_folder
     def input(self):
         keys = pygame.key.get_pressed()
 
+        # movement input
         if keys[pygame.K_z]:
             self.direction.y = -1
         elif keys[pygame.K_s]:
@@ -28,6 +44,18 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = -1
         else:
             self.direction.x = 0
+
+        # attack input
+        if keys[pygame.K_SPACE] and not self.attacking: # à changer avec le clic gauche de la souris
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print('attack')
+
+        # magic input
+        if keys[pygame.K_LCTRL] and not self.attacking: # à changer avec le clic droit de la souris
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print('magic')
 
     def move(self, speed):
         if self.direction.magnitude() != 0:
@@ -55,6 +83,14 @@ class Player(pygame.sprite.Sprite):
                         self.rect.bottom = sprite.rect.top
                     if self.direction.y < 0: #moving top
                         self.rect.top = sprite.rect.bottom
+
+    def cooldowns(self):
+        current_time = pygame.time.get_ticks()
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
+
     def update(self):
         self.input()
+        self.cooldowns()
         self.move(self.speed)
