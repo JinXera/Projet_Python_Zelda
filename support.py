9054b8c5cc450d13f3e61fc7b8fafa02dd2e1d71
JinXera <baptiste.salvio@stjoavignon.com>
2024-04-12 17:24:08 +0200

    print(animation) dans player.py

--- support.py
+++ support.py
@@ -3,9 +3,9 @@ import pygame
 
 def import_folder(folder_path):
     images = []
-    for filename in os.listdir(folder_path):
-        filepath = os.path.join(folder_path, filename)
-        if os.path.isfile(filepath) and filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            image = pygame.image.load(filepath).convert_alpha()
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.isfile(file_path) and file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image = pygame.image.load(file_path).convert_alpha()
             images.append(image)
     return images
