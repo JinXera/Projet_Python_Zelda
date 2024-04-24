3bf609bb7e7876ef865d77ed3b8eb556f65baaa2
JinXera <baptiste.salvio@stjoavignon.com>
2024-04-24 22:23:38 +0200

    Partie weapon terminée, modification dans les parties level.py, player.py et weapon.py

--- level.py
+++ level.py
@@ -5,6 +5,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from weapon import Weapon
 
 class Level :
     def __init__(self):
@@ -16,8 +17,12 @@ class Level :
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        # attack sprites
+        self.current_attack = None
+
         # sprite setup
         self.create_map()
+
     def create_map(self):
         layout = {
             'boundary': import_csv_layout('../Projet_Python_Zelda/map/map_FloorBlocks.csv'),
@@ -48,15 +53,23 @@ class Level :
                             surf = graphics['objects'][int(col)]
                             Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
 
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
 
+    def create_attack(self):
+        self.current_attack = Weapon(self.player,[self.visible_sprites])
 
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
 
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.status)
+
+
 
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -87,4 +100,3 @@ class YSortCameraGroup(pygame.sprite.Group):
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
-
