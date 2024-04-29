ab2dc361d528c020f879e7ce0ffab549dfb7f7c1
JinXera <baptiste.salvio@stjoavignon.com>
2024-04-29 09:03:03 +0200

    Partie user interface terminé

--- ui.py
+++ ui.py
@@ -12,6 +12,13 @@ class UI:
         self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
+        # convert weapon dictionary
+        self.weapon_graphics = []
+        for weapon in weapon_data.values():
+            path = weapon['graphic']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+
     def show_bar(self,current,max_amount,bg_rect,color):
         # draw bg
         pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
@@ -32,10 +39,31 @@ class UI:
         y = self.display_surface.get_size()[1] - 20
         text_rect = text_surf.get_rect(bottomright = (x,y))
 
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, text_rect.inflate(20,20))
         self.display_surface.blit(text_surf, text_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, text_rect.inflate(20, 20), 3)
+
+    def selection_box(self, left, top, has_switched):
+        bg_rect = pygame.Rect(left, top, ITEM_BOX_SIZE, ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+        if has_switched :
+            pygame.draw.rect(self.display_surface, UI_BORDER_COLOR_ACTIVE, bg_rect, 3)
+        else:
+            pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
+        return bg_rect
+
+    def weapon_overlay(self, weapon_index, has_switched):
+        bg_rect = self.selection_box(10, 630, has_switched)  # weapon
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center = bg_rect.center)
+
+        self.display_surface.blit(weapon_surf, weapon_rect)
 
     def display(self, player):
         self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
         self.show_bar(player.health, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
 
         self.show_exp(player.exp)
+
+        self.weapon_overlay(player.weapon_index, not player.can_switch_weapon)
+        # self.selection_box(80, 635) # magic
