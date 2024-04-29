fc30cd84aa4f7520e4b86efaae3723cf8804c687
Cyrine <cyrine.aramouny@gmail.com>
2024-04-29 09:18:12 +0200

    création de entity.py avec les fonctions move et collision (qui était dans player.py) pour les réutiliser pour les mouvements de l'ennemi.

--- player.py
+++ player.py
@@ -1,22 +1,21 @@
 import pygame
 from settings import *
 from support import import_folder
+from entity import Entity
 
-class Player(pygame.sprite.Sprite):
+
+class Player(Entity):
     def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack):
         super().__init__(groups)
         self.image = pygame.image.load('../Projet_Python_Zelda/graphics/test/player.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -26)
 
-        #graphics setup
+        # graphics setup
         self.import_player_assets()
         self.status = 'down'
-        self.frame_index = 0
-        self.animation_speed = 0.15
 
         # movement
-        self.direction = pygame.math.Vector2()
         self.attacking = False
         self.attack_cooldown = 400
         self.attack_time = None
@@ -28,7 +27,7 @@ class Player(pygame.sprite.Sprite):
         self.weapon_index = 0
         self.weapon = list(weapon_data.keys())[self.weapon_index]
         self.can_switch_weapon = True
-        self.weapons_switch_time =None
+        self.weapons_switch_time = None
         self.switch_duration_cooldown = 200
 
         # stats
@@ -41,8 +40,8 @@ class Player(pygame.sprite.Sprite):
     def import_player_assets(self):
         character_path = '../Projet_Python_Zelda/graphics/player/'
         self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
-                          'right_idle':[], 'left_idle':[], 'up_idle':[], 'down_idle':[],
-                          'right_attack':[], 'left_attack':[], 'up_attack':[], 'down_attack':[]}
+                          'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                          'right_attack': [], 'left_attack': [], 'up_attack': [], 'down_attack': []}
 
         for animation in self.animations.keys():
             full_path = character_path + animation
@@ -113,33 +112,6 @@ class Player(pygame.sprite.Sprite):
             if 'attack' in self.status:
                 self.status = self.status.replace('_attack', '')
 
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-
-        self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
-        #self.rect.center += self.direction * speed
-        self.rect.center = self.hitbox.center
-
-    def collision(self, direction):
-        if direction == 'horizontal' :
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0: #moving right
-                        self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x < 0: #moving left
-                        self.hitbox.left = sprite.hitbox.right
-
-        if direction == 'vertical' :
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0: #moving down
-                        self.hitbox.bottom = sprite.hitbox.top
-                    if self.direction.y < 0: #moving top
-                        self.hitbox.top = sprite.hitbox.bottom
 
     def cooldowns(self):
         current_time = pygame.time.get_ticks()
